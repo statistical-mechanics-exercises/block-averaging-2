e9613600fdea3472c5d09d5087f4dd45955107a6
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 18:57:45 +0000

    Added answer

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ eng = np.loadtxt("energies")[:,1]
 av_eng = 10*[0]
 
 # Your code goes here
+bsize = int( len(eng) / 10 )
+for i in range(10) :
+    av_eng[i] = sum( eng[bsize*i:bsize*(i+1)] ) / bsize
 
 
 # This will draw a graph of your energies
